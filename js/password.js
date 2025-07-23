63140480a06a8112ebe4b9ff121f173e1c2b9f40
Matsuyu-Archive <yuie960012@gmail.com>
2025-07-24 04:29:16 +0900

    git push origin main

--- js/password.js
+++
@@ -1,8 +0,0 @@
-function checkPassword() {
-  const input = document.getElementById("passwordInput").value;
-  if (input === "0112") {
-    window.location.href = "home.html";
-  } else {
-    alert("비밀번호가 틀렸습니다.");
-  }
-}
